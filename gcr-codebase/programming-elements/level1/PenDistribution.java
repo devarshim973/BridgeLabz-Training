b78ae04b20c5e35601820aa2900fa953ffe80847
Devarshi Mishra <153836165+devarshim973@users.noreply.github.com>
2025-12-17 21:41:45 +0530

    [devarshi] PenDistribution.java

--- gcr-codebase/programming-elements/level1/PenDistribution.java
+++ gcr-codebase/programming-elements/level1/PenDistribution.java
@@ -3,10 +3,14 @@ import java.util.*;
 
 public class PenDistribution {
     public static void main(String[] args) {
+
+        // Total number of pens available
         int totalPens = 14;
         int students = 3;
-
+        
+          // Calculate number of pens each student
         int pensPerStudent = totalPens / students;
+        // Calculate remaining pens
         int remainingPens = totalPens % students;
 
         System.out.println("The Pen Per Student is " + pensPerStudent +
