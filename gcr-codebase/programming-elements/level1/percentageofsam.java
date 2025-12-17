0d782923e85fd14f9e07eaf31f8886dac1552aee
devarshim973 <devarshim973@gmail.com>
2025-12-17 12:28:56 +0530

    [devarshi]percentageofsam

---
+++ gcr-codebase/programming-elements/level1/percentageofsam.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class percentageofsam{
+	public static void main(String[] args){
+		int math = 94;
+		int physics = 95;
+		int chemistry = 96;
+		
+		int totalmarks = math + physics + chemistry;
+		double average = totalmarks/3.0;
+		
+		System.out.println("Sam’s average mark in PCM is " + average);
+	}
+}
+		
