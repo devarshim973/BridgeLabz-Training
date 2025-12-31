e807ae908ecbc7a1c290d03af705cca0c6fb2224
devarshim973 <devarshim973@gmail.com>
2025-12-31 16:27:11 +0530

    [devarshi]Manager.java

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/Inheritance/Manager.java
@@ -0,0 +1,16 @@
+package com.Inheritance;
+
+public class Manager extends Employee {
+    int teamSize;
+
+    public Manager(String name, int id, double salary, int teamSize) {
+        super(name, id, salary);
+        this.teamSize = teamSize;
+    }
+
+    @Override
+    void displayDetails() {
+        super.displayDetails();
+        System.out.println("Role: Manager, Team Size: " + teamSize);
+    }
+}
