98d3cd4d77d8c93b58b200960fd2206ab44e9199
devarshim973 <devarshim973@gmail.com>
2025-12-31 16:24:28 +0530

    [devarshi]Developer.java

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/Inheritance/Developer.java
@@ -0,0 +1,16 @@
+package com.Inheritance;
+
+public class Developer extends Employee {
+    String programmingLanguage;
+
+    public Developer(String name, int id, double salary, String programmingLanguage) {
+        super(name, id, salary);
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    @Override
+    void displayDetails() {
+        super.displayDetails();
+        System.out.println("Role: Developer, Programming Language: " + programmingLanguage);
+    }
+}
