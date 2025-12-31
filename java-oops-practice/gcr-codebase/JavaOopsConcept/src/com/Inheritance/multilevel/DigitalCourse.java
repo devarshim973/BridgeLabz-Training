7e4a4748515116fc51f611c45bff18b19dc1d136
devarshim973 <devarshim973@gmail.com>
2025-12-31 17:03:20 +0530

    [devarshi]DigitalCourse.java

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/Inheritance/multilevel/DigitalCourse.java
@@ -0,0 +1,19 @@
+package com.Inheritance.multilevel;
+
+public class DigitalCourse extends StudyCourse {
+    String platform;
+    boolean isRecorded;
+
+    public DigitalCourse(String courseName, int duration, String platform, boolean isRecorded) {
+        super(courseName, duration);
+        this.platform = platform;
+        this.isRecorded = isRecorded;
+    }
+
+    @Override
+    void displayCourseInfo() {
+        super.displayCourseInfo();
+        System.out.println("Platform: " + platform);
+        System.out.println("Recorded: " + (isRecorded ? "Yes" : "No"));
+    }
+}
