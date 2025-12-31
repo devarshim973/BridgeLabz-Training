5fcdd1f06bf7e1653ef58ab5b9a95af7592ac24d
devarshim973 <devarshim973@gmail.com>
2025-12-31 16:57:11 +0530

    [devarshi]RetailOrder.java

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/Inheritance/multilevel/RetailOrder.java
@@ -0,0 +1,22 @@
+package com.Inheritance.multilevel;
+
+public class RetailOrder {
+    int orderId;
+    String orderDate;
+
+    public RetailOrder(int orderId, String orderDate) {
+        this.orderId = orderId;
+        this.orderDate = orderDate;
+    }
+
+    // Method to get order status
+    String getOrderStatus() {
+        return "Order Placed";
+    }
+
+    void displayOrderInfo() {
+        System.out.println("Order ID: " + orderId);
+        System.out.println("Order Date: " + orderDate);
+        System.out.println("Status: " + getOrderStatus());
+    }
+}
