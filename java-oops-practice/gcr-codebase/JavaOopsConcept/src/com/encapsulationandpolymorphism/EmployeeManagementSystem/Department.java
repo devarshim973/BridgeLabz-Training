3b69b8597d8e481f73ce9336dd4e99fb6cd1372d
devarshim973 <devarshim973@gmail.com>
2026-01-01 15:15:29 +0530

    [devarshi]Department.java

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/encapsulationandpolymorphism/EmployeeManagementSystem/Department.java
@@ -0,0 +1,8 @@
+package com.encapsulationandpolymorphism.EmployeeManagementSystem;
+
+public interface Department {
+
+    void assignDepartment(String departmentName);
+
+    String getDepartmentDetails();
+}
