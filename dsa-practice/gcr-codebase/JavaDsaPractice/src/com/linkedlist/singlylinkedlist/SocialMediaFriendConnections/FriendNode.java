017849dd8512312bfe8c6f414f121f5858b65e61
devarshim973 <devarshim973@gmail.com>
2026-01-02 16:28:28 +0530

    [devarshi]FriendNode.java

---
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/com/linkedlist/singlylinkedlist/SocialMediaFriendConnections/FriendNode.java
@@ -0,0 +1,11 @@
+package com.linkedlist.singlylinkedlist.SocialMediaFriendConnections;
+
+public class FriendNode {
+    int friendId;
+    FriendNode next;
+
+    public FriendNode(int friendId) {
+        this.friendId = friendId;
+        this.next = null;
+    }
+}
