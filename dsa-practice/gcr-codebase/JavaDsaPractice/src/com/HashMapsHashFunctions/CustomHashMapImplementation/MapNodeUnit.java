5cffd833049a2e17857c77cafc7c08a5d84225bd
devarshim973 <devarshim973@gmail.com>
2026-01-03 16:14:19 +0530

    [devarshi] MapNodeUnit.java

---
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/com/HashMapsHashFunctions/CustomHashMapImplementation/MapNodeUnit.java
@@ -0,0 +1,15 @@
+package com.HashMapsHashFunctions.CustomHashMapImplementation;
+
+public class MapNodeUnit {
+
+    int key;
+    String value;
+    MapNodeUnit next;
+
+    public MapNodeUnit(int key, String value) {
+        this.key = key;
+        this.value = value;
+        this.next = null;
+    }
+}
+
