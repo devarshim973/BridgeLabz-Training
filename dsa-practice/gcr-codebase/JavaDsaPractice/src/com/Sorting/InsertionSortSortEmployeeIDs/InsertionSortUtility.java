b9996700de0e89634b2c9b32cbe0c0d4a4dfac8e
devarshim973 <devarshim973@gmail.com>
2026-01-05 17:03:01 +0530

    [devarshi]InsertionSortUtility.java

---
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/com/Sorting/InsertionSortSortEmployeeIDs/InsertionSortUtility.java
@@ -0,0 +1,25 @@
+package com.Sorting.InsertionSortSortEmployeeIDs;
+
+public class InsertionSortUtility {
+
+    public static void sortAscending(int[] employeeIds) {
+
+        int n = employeeIds.length;
+
+        // Start from second element
+        for (int i = 1; i < n; i++) {
+
+            int key = employeeIds[i];
+            int j = i - 1;
+
+            // Shift elements greater than key
+            while (j >= 0 && employeeIds[j] > key) {
+                employeeIds[j + 1] = employeeIds[j];
+                j--;
+            }
+
+            // Insert key at correct position
+            employeeIds[j + 1] = key;
+        }
+    }
+}
