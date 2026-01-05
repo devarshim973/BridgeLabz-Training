a0d8586eea24f5dfc3f12cc780d2ee0531bb9634
devarshim973 <devarshim973@gmail.com>
2026-01-05 16:39:07 +0530

    [devarshi]onPerishableProduct.java

---
+++ java-oops-practice/Scenario based-codebased/JavaScenarioBased/src/com/day4/onPerishableProduct.java
@@ -0,0 +1,13 @@
+package com.day4;
+
+class NonPerishableProduct extends Product {
+
+    public NonPerishableProduct(String name, double price) {
+        super(name, price, "Non-Perishable");
+    }
+
+    @Override
+    public double getDiscountRate() {
+        return 0.05; // 5% discount
+    }
+}
