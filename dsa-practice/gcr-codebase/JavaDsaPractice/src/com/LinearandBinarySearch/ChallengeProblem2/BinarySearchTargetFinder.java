653fd358373cd20b44595312c55d2d6f44e0f750
devarshim973 <devarshim973@gmail.com>
2026-01-06 17:21:02 +0530

    [devarshi]BinarySearchTargetFinder.java

---
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/com/LinearandBinarySearch/ChallengeProblem2/BinarySearchTargetFinder.java
@@ -0,0 +1,30 @@
+package com.LinearandBinarySearch.ChallengeProblem2;
+
+import java.util.Arrays;
+
+public class BinarySearchTargetFinder {
+
+    // Find index of target in sorted array
+    public int findTargetIndex(int[] nums, int target) {
+
+        Arrays.sort(nums); // Sort array first
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1; // Target not found
+    }
+}
