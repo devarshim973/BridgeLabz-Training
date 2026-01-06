cd421c1d1893a11de57f492cf56af9c0cfc26c8b
devarshim973 <devarshim973@gmail.com>
2026-01-06 22:48:40 +0530

    [devarshi]EventUser.java

---
+++ java-oops-practice/Scenario based-codebased/JavaScenarioBased/src/com/Day5/EventEaseEventManagementPlatform/EventUser.java
@@ -0,0 +1,16 @@
+package com.Day5.EventEaseEventManagementPlatform;
+
+public class EventUser {
+
+    private String userName;
+    private String email;
+
+    public EventUser(String userName, String email) {
+        this.userName = userName;
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+}
