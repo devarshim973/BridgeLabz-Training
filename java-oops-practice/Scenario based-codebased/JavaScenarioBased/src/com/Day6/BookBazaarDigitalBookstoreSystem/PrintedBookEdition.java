46f8420d042869ccdd64e33969251029ba82901d
devarshim973 <devarshim973@gmail.com>
2026-01-07 16:06:47 +0530

    [devarshi]PrintedBookEdition.java

---
+++ java-oops-practice/Scenario based-codebased/JavaScenarioBased/src/com/Day6/BookBazaarDigitalBookstoreSystem/PrintedBookEdition.java
@@ -0,0 +1,17 @@
+package com.Day6.BookBazaarDigitalBookstoreSystem;
+
+class PrintedBookEdition extends BookBaseItem {
+
+    public PrintedBookEdition(String title, String author, double price, int stock) {
+        super(title, author, price, stock);
+    }
+
+    @Override
+    public double applyDiscount(int quantity) {
+        // Buy 2 get 5% discount
+        if (quantity >= 2) {
+            return price * quantity * 0.05;
+        }
+        return 0;
+    }
+}
