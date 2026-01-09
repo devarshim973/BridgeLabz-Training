68e2304407fb4afe9d3a7e86ff9d6483cbba9cfb
devarshim973 <devarshim973@gmail.com>
2026-01-09 16:19:41 +0530

    [devarshi]VegFoodItem.java

---
+++ java-oops-practice/Scenario based-codebased/JavaScenarioBased/src/com/Day8/FoodLoopRestaurantOrderingSystem/VegFoodItem.java
@@ -0,0 +1,8 @@
+package com.Day8.FoodLoopRestaurantOrderingSystem;
+
+class VegFoodItem extends BaseFoodItem {
+
+    public VegFoodItem(String name, double price, int stock) {
+        super(name, "Veg", price, stock);
+    }
+}
