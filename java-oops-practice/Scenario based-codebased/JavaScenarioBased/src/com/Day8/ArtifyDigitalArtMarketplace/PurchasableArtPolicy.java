750508bf7f428dba21b7170085ac47629e85adc3
devarshim973 <devarshim973@gmail.com>
2026-01-09 13:29:15 +0530

    [devarshi]PurchasableArtPolicy.java

---
+++ java-oops-practice/Scenario based-codebased/JavaScenarioBased/src/com/Day8/ArtifyDigitalArtMarketplace/PurchasableArtPolicy.java
@@ -0,0 +1,8 @@
+package com.Day8.ArtifyDigitalArtMarketplace;
+
+interface PurchasableArtPolicy {
+
+    void purchase(MarketplaceUserProfile user);
+
+    void licenseArt(MarketplaceUserProfile user);
+}
