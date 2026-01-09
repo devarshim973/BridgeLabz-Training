077ddb71a1bbde3ba256e742b2889aabb77e9069
devarshim973 <devarshim973@gmail.com>
2026-01-09 16:52:37 +0530

    [devarshi]  InstructorUserProfile.java

---
+++ java-oops-practice/Scenario based-codebased/JavaScenarioBased/src/com/Day8/SkillForgeOnlineLearningPortal/InstructorUserProfile.java
@@ -0,0 +1,8 @@
+package com.Day8.SkillForgeOnlineLearningPortal;
+
+class InstructorUserProfile extends PortalUserProfile {
+
+    public InstructorUserProfile(String name, String email) {
+        super(name, email);
+    }
+}
