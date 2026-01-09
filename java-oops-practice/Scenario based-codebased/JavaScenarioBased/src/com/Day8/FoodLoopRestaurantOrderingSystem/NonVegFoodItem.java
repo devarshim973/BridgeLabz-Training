45b856973cd9d05925789d9c2aa66c8e873e17ef
devarshim973 <devarshim973@gmail.com>
2026-01-09 16:18:49 +0530

    [devarshi]NonVegFoodItem.java

---
+++ java-oops-practice/Scenario based-codebased/JavaScenarioBased/src/com/Day8/FoodLoopRestaurantOrderingSystem/NonVegFoodItem.java
@@ -0,0 +1,8 @@
+package com.Day8.FoodLoopRestaurantOrderingSystem;
+
+class NonVegFoodItem extends BaseFoodItem {
+
+    public NonVegFoodItem(String name, double price, int stock) {
+        super(name, "Non-Veg", price, stock);
+    }
+}
