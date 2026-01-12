71c06e2683ff89120a3c4ebd529417546b2a0c2a
devarshim973 <devarshim973@gmail.com>
2026-01-12 23:44:10 +0530

    [devarshi]SortedListNode.java

---
+++ dsa-practice/Leet-code-codebase/javadsaleetcodepractice/src/com/day1/MergeTwoSortedLists/SortedListNode.java
@@ -0,0 +1,12 @@
+package com.day1.MergeTwoSortedLists;
+
+class SortedListNode {
+
+    int value;
+    SortedListNode next;
+
+    public SortedListNode(int value) {
+        this.value = value;
+        this.next = null;
+    }
+}
