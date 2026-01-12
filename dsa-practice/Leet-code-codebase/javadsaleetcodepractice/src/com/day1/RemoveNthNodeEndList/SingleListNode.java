b87d9f4ef2463197574baf1f70cb50e5a8fac3ba
devarshim973 <devarshim973@gmail.com>
2026-01-12 23:01:46 +0530

    [devarshi]SingleListNode.java

---
+++ dsa-practice/Leet-code-codebase/javadsaleetcodepractice/src/com/day1/RemoveNthNodeEndList/SingleListNode.java
@@ -0,0 +1,12 @@
+package com.day1.RemoveNthNodeEndList;
+
+class SingleListNode {
+
+    int value;
+    SingleListNode next;
+
+    public SingleListNode(int value) {
+        this.value = value;
+        this.next = null;
+    }
+}
