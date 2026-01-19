a3ad1d0add5760676271785b066edc50c3e6e58a
devarshim973 <devarshim973@gmail.com>
2026-01-19 16:36:10 +0530

    [devarshi]ListNodeElement.java

---
+++ dsa-practice/Leet-code-codebase/javadsaleetcodepractice/src/com/Day2/MergekSortedLinkedLists/ListNodeElement.java
@@ -0,0 +1,12 @@
+package com.Day2.MergekSortedLinkedLists;
+
+public class ListNodeElement {
+
+    int value;
+    ListNodeElement next;
+
+    public ListNodeElement(int value) {
+        this.value = value;
+        this.next = null;
+    }
+}
