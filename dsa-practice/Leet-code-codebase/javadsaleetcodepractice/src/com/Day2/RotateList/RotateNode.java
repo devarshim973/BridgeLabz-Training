c8b36444b1eedbb7ea0d5ecfe9895a9f1c13770c
devarshim973 <devarshim973@gmail.com>
2026-01-19 16:38:28 +0530

    [devarshi]RotateNode.java

---
+++ dsa-practice/Leet-code-codebase/javadsaleetcodepractice/src/com/Day2/RotateList/RotateNode.java
@@ -0,0 +1,12 @@
+package com.Day2.RotateList;
+
+public class RotateNode {
+
+    int data;
+    RotateNode next;
+
+    public RotateNode(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
