b56d6f65eddd260450b833a3aa522a311f531d40
devarshim973 <devarshim973@gmail.com>
2026-01-21 00:54:01 +0530

    [devarshi]trywithresources

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/Exceptions/trywithresources/TryWithResourcesExample.java
@@ -0,0 +1,21 @@
+package com.Exceptions.trywithresources;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TryWithResourcesExample {
+
+    public static void main(String[] args) {
+
+        // try-with-resources automatically closes BufferedReader
+        try (BufferedReader br = new BufferedReader(new FileReader("info.txt"))) {
+
+            String firstLine = br.readLine();
+            System.out.println(firstLine);
+
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+        }
+    }
+}
