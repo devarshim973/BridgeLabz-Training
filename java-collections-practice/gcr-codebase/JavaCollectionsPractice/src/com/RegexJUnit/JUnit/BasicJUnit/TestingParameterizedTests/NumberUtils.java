529d42153043b3e1c3b313468ae7748aafa27460
devarshim973 <devarshim973@gmail.com>
2026-01-23 16:12:29 +0530

    [devarshi]NumberUtils

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/RegexJUnit/JUnit/BasicJUnit/TestingParameterizedTests/NumberUtils.java
@@ -0,0 +1,8 @@
+package com.RegexJUnit.JUnit.BasicJUnit.TestingParameterizedTests;
+
+public class NumberUtils {
+
+    public boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+}
