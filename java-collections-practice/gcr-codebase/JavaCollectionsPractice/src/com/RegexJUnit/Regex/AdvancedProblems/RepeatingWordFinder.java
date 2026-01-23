27d1cea9c738fad7c0f61f5167ee3136b4843fb1
devarshim973 <devarshim973@gmail.com>
2026-01-23 14:25:06 +0530

    [devarshi]RepeatingWordFinder.java

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/RegexJUnit/Regex/AdvancedProblems/RepeatingWordFinder.java
@@ -0,0 +1,20 @@
+package com.RegexJUnit.Regex.AdvancedProblems;
+
+import java.util.regex.*;
+
+class RepeatingWordFinder {
+
+    public static void main(String[] args) {
+
+        String text = "This is is a repeated repeated word test.";
+
+        String regex = "\\b(\\w+)\\b\\s+\\1\\b";
+
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(text);
+
+        while (matcher.find()) {
+            System.out.println(matcher.group(1));
+        }
+    }
+}
