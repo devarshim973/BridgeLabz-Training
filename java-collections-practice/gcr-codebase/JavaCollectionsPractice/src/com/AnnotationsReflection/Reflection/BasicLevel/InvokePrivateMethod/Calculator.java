49d722bfc07f515740fe875832874128f7d4bb9c
devarshim973 <devarshim973@gmail.com>
2026-01-25 00:05:27 +0530

    [devarshi]Calculator

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/AnnotationsReflection/Reflection/BasicLevel/InvokePrivateMethod/Calculator.java
@@ -0,0 +1,8 @@
+package com.AnnotationsReflection.Reflection.BasicLevel.InvokePrivateMethod;
+
+public class Calculator {
+
+    private int multiply(int a, int b) {
+        return a * b;
+    }
+}
