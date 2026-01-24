33fd055979bc02ac402149ff6312ec3849134cc4
devarshim973 <devarshim973@gmail.com>
2026-01-25 00:09:04 +0530

    [devarshi]AnnotatedBook

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/AnnotationsReflection/Reflection/IntermediateLevel/RetrieveAnnotationsRuntime/AnnotatedBook.java
@@ -0,0 +1,10 @@
+package com.AnnotationsReflection.Reflection.IntermediateLevel.RetrieveAnnotationsRuntime;
+
+
+@Author(name = "Author Name")
+public class AnnotatedBook {
+
+    public void display() {
+        System.out.println("Book class annotated with Author");
+    }
+}
