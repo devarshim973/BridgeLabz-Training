7b5ee0f5a0df93b9b2e917dfb831198747886a0f
devarshim973 <devarshim973@gmail.com>
2026-01-24 23:49:39 +0530

    [devarshi]StudentRecord.java

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/AnnotationsReflection/Reflection/AdvancedLevel/CreateCustomObjectMapper/StudentRecord.java
@@ -0,0 +1,15 @@
+package com.AnnotationsReflection.Reflection.AdvancedLevel.CreateCustomObjectMapper;
+
+
+public class StudentRecord {
+
+    private int id;
+    private String name;
+    private double marks;
+
+    public void display() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Marks: " + marks);
+    }
+}
