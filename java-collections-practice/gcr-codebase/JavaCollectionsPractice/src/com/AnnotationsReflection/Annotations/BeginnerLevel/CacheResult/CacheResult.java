10a545e5d97e6078ddc3d3e062959cfb4dcdf705
devarshim973 <devarshim973@gmail.com>
2026-01-25 22:19:38 +0530

    [devarshi]CacheResult.java

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/AnnotationsReflection/Annotations/BeginnerLevel/CacheResult/CacheResult.java
@@ -0,0 +1,12 @@
+package com.AnnotationsReflection.Annotations.BeginnerLevel.CacheResult;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+
+// Method-level annotation, available at runtime
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface CacheResult {
+}
