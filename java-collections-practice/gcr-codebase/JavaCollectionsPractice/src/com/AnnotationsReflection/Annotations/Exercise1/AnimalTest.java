eb282ebf3f9cb0bdb8b2fd7054eade19bb72f378
devarshim973 <devarshim973@gmail.com>
2026-01-25 22:30:59 +0530

    [devarshi]AnimalTest.java

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/AnnotationsReflection/Annotations/Exercise1/AnimalTest.java
@@ -0,0 +1,14 @@
+package com.AnnotationsReflection.Annotations.Exercise1;
+
+
+public class AnimalTest {
+
+    public static void main(String[] args) {
+
+        // Create Dog object
+        Dog dog = new Dog();
+
+        // Call overridden method
+        dog.makeSound();
+    }
+}
