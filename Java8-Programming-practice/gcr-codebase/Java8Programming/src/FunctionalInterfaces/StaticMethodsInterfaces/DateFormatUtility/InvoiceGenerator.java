07aa22cebfd046b80891cd2d4bf35ea0f3628113
devarshim973 <devarshim973@gmail.com>
2026-01-30 22:22:28 +0530

    [devarshi]InvoiceGenerator.java

---
+++ Java8-Programming-practice/gcr-codebase/Java8Programming/src/FunctionalInterfaces/StaticMethodsInterfaces/DateFormatUtility/InvoiceGenerator.java
@@ -0,0 +1,17 @@
+package FunctionalInterfaces.StaticMethodsInterfaces.DateFormatUtility;
+
+import java.time.LocalDate;
+
+public class InvoiceGenerator {
+
+    public static void main(String[] args) {
+
+        LocalDate invoiceDate = LocalDate.now();
+
+        System.out.println("ISO Date      : " + DateFormatUtils.formatISO(invoiceDate));
+        System.out.println("Invoice Date  : " + DateFormatUtils.formatForInvoice(invoiceDate));
+        System.out.println("US Date       : " + DateFormatUtils.formatUS(invoiceDate));
+        System.out.println("Custom Date   : " +
+                DateFormatUtils.formatCustom(invoiceDate, "dd MMM yyyy"));
+    }
+}
