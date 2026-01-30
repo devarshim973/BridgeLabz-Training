e7d2ac7a717528e314e95bc15011c939c952019b
devarshim973 <devarshim973@gmail.com>
2026-01-30 22:04:11 +0530

    [devarshi]SmartHomeController.java

---
+++ Java8-Programming-practice/gcr-codebase/Java8Programming/src/FunctionalInterfaces/ImplementingInterfaces/SmartDeviceControlInterface/SmartHomeController.java
@@ -0,0 +1,19 @@
+package FunctionalInterfaces.ImplementingInterfaces.SmartDeviceControlInterface;
+
+public class SmartHomeController {
+
+    public static void main(String[] args) {
+
+        SmartDevice light = new Light();
+        SmartDevice ac = new AirConditioner();
+        SmartDevice tv = new Television();
+
+        light.turnOn();
+        ac.turnOn();
+        tv.turnOn();
+
+        light.turnOff();
+        ac.turnOff();
+        tv.turnOff();
+    }
+}
