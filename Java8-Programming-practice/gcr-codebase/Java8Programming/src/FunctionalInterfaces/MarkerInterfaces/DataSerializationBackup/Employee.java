20fdeb0f26680e750bb231311c8185b835269fec
devarshim973 <devarshim973@gmail.com>
2026-01-30 22:13:27 +0530

    [devarshi]Employee.java

---
+++ Java8-Programming-practice/gcr-codebase/Java8Programming/src/FunctionalInterfaces/MarkerInterfaces/DataSerializationBackup/Employee.java
@@ -0,0 +1,17 @@
+package FunctionalInterfaces.MarkerInterfaces.DataSerializationBackup;
+
+public class Employee implements BackupSerializable {
+
+    private int id;
+    private String name;
+
+    public Employee(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{id=" + id + ", name='" + name + "'}";
+    }
+}
