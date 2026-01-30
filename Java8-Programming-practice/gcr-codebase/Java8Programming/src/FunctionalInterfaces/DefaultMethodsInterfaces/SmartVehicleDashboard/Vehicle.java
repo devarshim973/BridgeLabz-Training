09869d5519468fda4033dbfd442f1c3f8f1705f9
devarshim973 <devarshim973@gmail.com>
2026-01-30 17:22:08 +0530

    [devarshi]Vehicle.java

---
+++ Java8-Programming-practice/gcr-codebase/Java8Programming/src/FunctionalInterfaces/DefaultMethodsInterfaces/SmartVehicleDashboard/Vehicle.java
@@ -0,0 +1,13 @@
+package FunctionalInterfaces.DefaultMethodsInterfaces.SmartVehicleDashboard;
+
+public interface Vehicle {
+
+    // Common method all vehicles must implement
+    void displaySpeed();
+
+    // New feature for battery display (default method)
+    default void displayBattery() {
+        // Default: vehicles without battery just show N/A
+        System.out.println("Battery: N/A");
+    }
+}
