29342159852d42e5a1a3accb91d141ed23bd603a
devarshim973 <devarshim973@gmail.com>
2026-01-30 16:39:22 +0530

    [devarshi]Alert

---
+++ Java8-Programming-practice/gcr-codebase/Java8Programming/src/LambdaExpressions/NotificationFiltering/Alert.java
@@ -0,0 +1,31 @@
+package LambdaExpressions.NotificationFiltering;
+
+public class Alert {
+
+    private String type;      // EMERGENCY, APPOINTMENT, PROMOTION
+    private String message;
+    private int priority;     // 1 = High, 2 = Medium, 3 = Low
+
+    public Alert(String type, String message, int priority) {
+        this.type = type;
+        this.message = message;
+        this.priority = priority;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    @Override
+    public String toString() {
+        return "Alert{" +
+                "type='" + type + '\'' +
+                ", priority=" + priority +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
