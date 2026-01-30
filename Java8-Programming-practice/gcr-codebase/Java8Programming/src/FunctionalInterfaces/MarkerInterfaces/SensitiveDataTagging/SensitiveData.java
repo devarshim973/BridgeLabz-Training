59f6ea209bd144b600365eb9344f766f06a75cb3
devarshim973 <devarshim973@gmail.com>
2026-01-30 22:19:05 +0530

    [devarshi]SensitiveData.java

---
+++ Java8-Programming-practice/gcr-codebase/Java8Programming/src/FunctionalInterfaces/MarkerInterfaces/SensitiveDataTagging/SensitiveData.java
@@ -0,0 +1,6 @@
+package FunctionalInterfaces.MarkerInterfaces.SensitiveDataTagging;
+
+//Marker interface – no methods
+public interface SensitiveData {
+ // No methods, just a marker for sensitive data
+}
