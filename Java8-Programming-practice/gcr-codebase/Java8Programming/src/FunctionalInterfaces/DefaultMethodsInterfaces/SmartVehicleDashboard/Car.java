c4dc81952acbda40be0b43857941d1e438bd59b1
devarshim973 <devarshim973@gmail.com>
2026-01-30 17:21:28 +0530

    [devarshi]Car

---
+++ Java8-Programming-practice/gcr-codebase/Java8Programming/src/FunctionalInterfaces/DefaultMethodsInterfaces/SmartVehicleDashboard/Car.java
@@ -0,0 +1,11 @@
+package FunctionalInterfaces.DefaultMethodsInterfaces.SmartVehicleDashboard;
+
+public class Car implements Vehicle {
+
+    @Override
+    public void displaySpeed() {
+        System.out.println("Car speed: 80 km/h");
+    }
+
+    // Uses default displayBattery() → N/A
+}
