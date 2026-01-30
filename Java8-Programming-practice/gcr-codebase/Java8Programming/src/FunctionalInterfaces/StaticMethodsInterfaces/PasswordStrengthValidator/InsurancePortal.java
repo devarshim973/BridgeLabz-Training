fb4f976e18691a576686413486a7b13b095fcb96
devarshim973 <devarshim973@gmail.com>
2026-01-30 22:23:01 +0530

    [devarshi]InsurancePortal

---
+++ Java8-Programming-practice/gcr-codebase/Java8Programming/src/FunctionalInterfaces/StaticMethodsInterfaces/PasswordStrengthValidator/InsurancePortal.java
@@ -0,0 +1,15 @@
+package FunctionalInterfaces.StaticMethodsInterfaces.PasswordStrengthValidator;
+
+public class InsurancePortal {
+
+    public static void main(String[] args) {
+
+        String password = "Ins@1234";
+
+        if (SecurityUtils.isStrongPassword(password)) {
+            System.out.println("✅ Strong password accepted");
+        } else {
+            System.out.println("❌ Weak password rejected");
+        }
+    }
+}
