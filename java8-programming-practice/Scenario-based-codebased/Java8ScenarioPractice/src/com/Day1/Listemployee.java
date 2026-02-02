910ba935c18bad8f180cd47fe090fee02fc35ac5
devarshim973 <devarshim973@gmail.com>
2026-02-03 00:20:37 +0530

    [devarshi]Listemployee.java

---
+++ java8-programming-practice/Scenario-based-codebased/Java8ScenarioPractice/src/com/Day1/Listemployee.java
@@ -0,0 +1,23 @@
+package com.Day1;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Listemployee {
+	public static void main(String[] args) {
+		List<Employee> employee= Arrays.asList(
+				new Employee(1, "Aman", 25, "Male", "IT", 2020, 50000),
+	            new Employee(2, "Neha", 28, "Female", "HR", 2019, 45000),
+	            new Employee(3, "Ayush", 30, "Male", "Finance", 2018, 60000),
+	            new Employee(4, "Pooja", 26, "Female", "manager", 2021, 48000),
+	            new Employee(5, "nikhil", 27, "Male", "accountent", 2022, 52000)
+				);
+	Map<String, List<String>> map=employee.stream()
+			.collect(Collectors.groupingBy(Employee::getDepartment,
+					Collectors.mapping(Employee::getName,Collectors.toList())));
+	System.out.println(map);
+		
+	}
+}
