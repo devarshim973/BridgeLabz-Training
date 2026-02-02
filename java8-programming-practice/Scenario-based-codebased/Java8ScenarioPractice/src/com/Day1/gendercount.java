3402c8b4c475f0f2b8813f3e23760863a21f9814
devarshim973 <devarshim973@gmail.com>
2026-02-03 00:07:48 +0530

    [devarshi]gendercount.java

---
+++ java8-programming-practice/Scenario-based-codebased/Java8ScenarioPractice/src/com/Day1/gendercount.java
@@ -0,0 +1,28 @@
+package com.Day1;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class gendercount {
+	public static void main(String[] args) {
+		List<Employee> employee= Arrays.asList(
+				new Employee(1, "Aman", 25, "Male", "IT", 2020, 50000),
+	            new Employee(2, "Neha", 28, "Female", "HR", 2019, 45000),
+	            new Employee(3, "Ayush", 30, "Male", "Finance", 2018, 60000),
+	            new Employee(4, "Pooja", 26, "Female", "IT", 2021, 48000),
+	            new Employee(5, "nikhil", 27, "Male", "HR", 2022, 52000)
+				);
+		
+		long malecount= employee.stream()
+				.filter(i->i.getGender().equals("Male"))
+				.count();
+		
+		long femalecount= employee.stream()
+				.filter(i->i.getGender().equals("Female"))
+				.count();
+		
+		System.out.println("malecount: "+malecount);
+		System.out.println("femalecount: "+femalecount);
+	}
+
+}
