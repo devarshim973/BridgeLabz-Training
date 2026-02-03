84471bc52941979386983a9888485c126485457f
devarshim973 <devarshim973@gmail.com>
2026-02-03 16:03:24 +0530

    [devarshi] FemaleRank.java

---
+++ java8-programming-practice/Scenario-based-codebased/Java8ScenarioPractice/src/com/Day2/FemaleRank.java
@@ -0,0 +1,25 @@
+package com.Day2;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+public class FemaleRank {
+	public static void main(String[] args) {
+
+        List<Student> student = Arrays.asList(
+                new Student(1, "Aman", 20, "Male", "Satna", 45, "9876543210"),
+                new Student(2, "vaishali", 21, "Female", "Rewa", 72, "9876543222"),
+                new Student(3, "Nikhil", 19, "Male", "Indore", 30, "9876543333"),
+                new Student(4, "parul", 26, "Female", "Mumbai", 55, "9876543444"),
+                new Student(5, "Devansh", 20, "Male", "Bhopal", 15, "9876543555")
+        );
+ student.stream()
+      .max(Comparator.comparingInt(Student::getRank))
+      .map(Student::getFirstName)
+ .ifPresent(System.out::println);
+  
+	 
+}
+}
