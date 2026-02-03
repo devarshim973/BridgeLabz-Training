9f12ef05edff6023bd60fbfad8056f643e6589b3
devarshim973 <devarshim973@gmail.com>
2026-02-03 15:57:58 +0530

    [devarshi] StartswithS.java

---
+++ java8-programming-practice/Scenario-based-codebased/Java8ScenarioPractice/src/com/Day2/StartswithS.java
@@ -0,0 +1,23 @@
+package com.Day2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class StartswithS {
+	public static void main(String[] args) {
+
+        List<Student> student = Arrays.asList(
+                new Student(1, "Aman", 20, "Male", "Satna", 45, "9876543210"),
+                new Student(2, "vaishali", 21, "Female", "Rewa", 72, "9876543222"),
+                new Student(3, "Nikhil", 19, "Male", "Indore", 30, "9876543333"),
+                new Student(4, "Sarul", 26, "Female", "Mumbai", 55, "9876543444"),
+                new Student(5, "Devansh", 20, "Male", "Bhopal", 15, "9876543555")
+        );
+       List<Student> map=student.stream()
+        		.filter(i->i.getFirstName().startsWith("S"))
+        		.collect(Collectors.toList());
+       
+       System.out.println(map);
+}}
