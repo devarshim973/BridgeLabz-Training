4f26451946e3e796d99630eeeb0b59d1bf206a33
devarshim973 <devarshim973@gmail.com>
2026-02-03 16:05:34 +0530

    [devarshi]CountDepartmentMember.java

---
+++ java8-programming-practice/Scenario-based-codebased/Java8ScenarioPractice/src/com/Day2/CountDepartmentMember.java
@@ -0,0 +1,27 @@
+package com.Day2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class CountDepartmentMember {
+	public static void main(String[] args) {
+
+    List<Student> student = Arrays.asList(
+            new Student(1, "Aman", 20, "Male", "Satna", 45, "9876543210"),
+            new Student(2, "vaishali", 21, "Female", "Rewa", 72, "9876543222"),
+            new Student(3, "Nikhil", 19, "Male", "Indore", 30, "9876543333"),
+            new Student(4, "parul", 26, "Female", "Mumbai", 55, "9876543444"),
+            new Student(5, "Devansh", 20, "Male", "Bhopal", 15, "9876543555"),
+            new Student(5, "Harshal", 20, "Male", "Bhopal", 15, "9876543555")
+            
+    );
+    Map<String,Long> map= student.stream()
+    		.collect(Collectors.groupingBy(Student::getFirstName,
+    				Collectors.counting()));
+    
+   System.out.println(map);
+    
+}
+}
