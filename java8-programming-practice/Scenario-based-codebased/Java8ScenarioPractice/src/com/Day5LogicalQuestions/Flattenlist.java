26f1fd3bddecce1cc9346b47da580b3ff32bb0d7
devarshim973 <devarshim973@gmail.com>
2026-02-05 13:08:43 +0530

    [devarshi] Flattenlist.java

--- java8-programming-practice/Scenario-based-codebased/Java8ScenarioPractice/src/com/Day5LogicalQuestions/Flattenlist.java
+++ java8-programming-practice/Scenario-based-codebased/Java8ScenarioPractice/src/com/Day5LogicalQuestions/Flattenlist.java
@@ -11,14 +11,10 @@ public class Flattenlist {
                 List.of(
                         List.of(1, 2),
                         List.of(3, 4),
-                        List.of(5)
-                );
+                        List.of(5) );
 
-        List<Integer> result =
-                list.stream()
-                    .flatMap(innerList -> innerList.stream())
-                    .collect(Collectors.toList());
-
-        System.out.println(result);
+      list.stream()
+      .flatMap(i->i.stream())
+      .forEach(System.out::println);
     }
 }
