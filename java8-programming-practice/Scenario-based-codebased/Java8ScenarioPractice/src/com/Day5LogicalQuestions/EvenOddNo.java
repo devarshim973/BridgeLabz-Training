7a9e4d40bbcdb2eda5382646ad434a8774961fda
devarshim973 <devarshim973@gmail.com>
2026-02-05 13:08:28 +0530

    [devarshi]EvenOddNo.java

--- java8-programming-practice/Scenario-based-codebased/Java8ScenarioPractice/src/com/Day5LogicalQuestions/EvenOddNo.java
+++ java8-programming-practice/Scenario-based-codebased/Java8ScenarioPractice/src/com/Day5LogicalQuestions/EvenOddNo.java
@@ -6,10 +6,12 @@ import java.util.stream.Collectors;
 public class EvenOddNo {
 public static void main(String[] args) {
 	List<Integer> number=List.of(1,2,3,4,5,6);
+	
 	List<Integer>evenNum= number.stream()
 	.filter(i->i%2==0)
 	.collect(Collectors.toList());
 	System.out.println(" evennum "+ evenNum);
+	
 	List<Integer>oddNum= number.stream()
 			.filter(i->i%2!=0)
 			.collect(Collectors.toList());
